745e915e79be2d746793008cf71d84cbbe061421
max.segale <max.segale@fcbcure.com>
2021-04-11 16:42:39 -0400

    Added animated loading message for request

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+  /*
+   * Adds the loading section to the page
+   * Hides the other sections on the page
+   */
+  function viewLoadingMsg() {
+    // Create loading section with waiting message
+    var loadingSection = $('<section class="loading"></section>');
+    loadingSection.append('<div class="loading-icon"><img src="img/loading_spinner.gif" alt="Loading"></div>');
+    loadingSection.append('<h2 class="loading-msg">Please wait a moment...</h2>');
+
+    // Hide the current sections on the page
+    $('section').hide();
+
+    // Add the new loading section to the page
+    $('.navbar').after(loadingSection);
+  }
+
   $("#btn-search").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
@@ -14,6 +31,9 @@ $(document).ready(function () {
     }
 
     if (x === true) {
+      // Show the waiting message for the request
+      viewLoadingMsg();
+
       document.querySelector('input[type="text"]').parentNode.classList.remove("error");
       const proxyurl = "";
       const url =
@@ -52,6 +72,9 @@ $(document).ready(function () {
 
 
       if (x === true) {
+        // Show the waiting message for the request
+        viewLoadingMsg();
+
         const proxyurl = "";
         const url =
           'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
